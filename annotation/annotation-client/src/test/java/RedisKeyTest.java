e6f2bdac4a0aa9e0baa17da797d98dfcb8e3d09b
thangvynam <thangvynam1808@gmail.com>
2020-12-16 17:36:16 +0700

    add test feature A

--- annotation/annotation-client/src/test/java/RedisKeyTest.java
+++ annotation/annotation-client/src/test/java/RedisKeyTest.java
@@ -8,6 +8,11 @@ import zp.cps.annotation.entity.RedisKey;
 public class RedisKeyTest {
 
     @Test
+    public void test_FeatureA() {
+
+    }
+
+    @Test
     public void test() {
         String key = RedisKey.genCPSEntityRedisKey(441, "0906368182");
         String expect = "CPS|441|0906368182";
